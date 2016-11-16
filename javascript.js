59587fe5ff1ce97b4065b5ddbcf26f292d964f9b
radiantsnipe <victor.yu.canada@gmail.com>
2016-11-16 03:38:30 -0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,44 @@
+window.onload = time;
+
+function time () {
+    var date = new Date();
+    var weekday = new Array(7);
+    weekday[0] =  "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    var month = new Array(12);
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June"; 
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    var hours = (date.getHours()) % 12;
+    if (hours == 0)
+        hours = hours + 12; 
+
+    var minutes = date.getMinutes();
+    if (minutes < 10)
+        minutes = "0" + minutes;
+
+    var ampm = "AM";
+    if (date.getHours() >= 12)
+        ampm = "PM"
+
+    var elem = document.getElementById('date');
+    elem.innerHTML = weekday[date.getDay()] + " " + month[date.getMonth()] + " " + date.getDate() + " " + date.getFullYear();
+    elem = document.getElementById('time');
+    elem.innerHTML = hours + ":" + minutes + " " + ampm; 
+}
---
+++ readme.txt
@@ -0,0 +1,16 @@
+Chrome Extension - New Newtab
+
+Replaces the default new tab page with one that has greater functionality.
+Built with html and css.
+
+Only compatible with chrome because it's a chrome extension...
+
+Functionality: 
+- Google search bar
+- Shortcut links to preset sites
+- Displays time of day
+- CSS Animations on page load
+
+Future improvements:
+- Improve customizability (change links and text)
+
